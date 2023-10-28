bf79c381dc23ddb416fc429470083b9f3e1ca296
ste46751 <stefanquarti060@gmail.com>
2023-10-28 11:07:16 +0200

    Creazione classe dell'indirizzo, indirizzo base privato, subnet, CDIR

---
+++ generatore indirizzi/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace generatore_indirizzi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputHost = new System.Windows.Forms.TextBox();
+            this.InputSottoreti = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lst_tabella = new System.Windows.Forms.ListBox();
+            this.bttn_crea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputHost
+            // 
+            this.InputHost.Location = new System.Drawing.Point(50, 45);
+            this.InputHost.Name = "InputHost";
+            this.InputHost.Size = new System.Drawing.Size(147, 20);
+            this.InputHost.TabIndex = 0;
+            // 
+            // InputSottoreti
+            // 
+            this.InputSottoreti.Location = new System.Drawing.Point(289, 45);
+            this.InputSottoreti.Name = "InputSottoreti";
+            this.InputSottoreti.Size = new System.Drawing.Size(147, 20);
+            this.InputSottoreti.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserisci numero host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Inserisci numero sottoreti";
+            // 
+            // lst_tabella
+            // 
+            this.lst_tabella.FormattingEnabled = true;
+            this.lst_tabella.Location = new System.Drawing.Point(50, 82);
+            this.lst_tabella.Name = "lst_tabella";
+            this.lst_tabella.Size = new System.Drawing.Size(445, 251);
+            this.lst_tabella.TabIndex = 4;
+            // 
+            // bttn_crea
+            // 
+            this.bttn_crea.Location = new System.Drawing.Point(555, 35);
+            this.bttn_crea.Name = "bttn_crea";
+            this.bttn_crea.Size = new System.Drawing.Size(87, 47);
+            this.bttn_crea.TabIndex = 5;
+            this.bttn_crea.Text = "Crea";
+            this.bttn_crea.UseVisualStyleBackColor = true;
+            this.bttn_crea.Click += new System.EventHandler(this.bttn_crea_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttn_crea);
+            this.Controls.Add(this.lst_tabella);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputSottoreti);
+            this.Controls.Add(this.InputHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputHost;
+        private System.Windows.Forms.TextBox InputSottoreti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lst_tabella;
+        private System.Windows.Forms.Button bttn_crea;
+    }
+}
+
---
+++ generatore indirizzi/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generatore_indirizzi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double Subnet(double bit_sotto)
+        {
+            double n = bit_sotto;
+            string indirizzo = "";
+            
+            for (int i = 0; i < n; i++)
+            {
+                indirizzo += "1";
+            }
+
+            for (double i = n; i < 8; i++)
+            {
+                indirizzo += "0";
+            }
+
+            double valore = 0;
+            double elevazione = 7;
+
+            for (int i = 0; i < indirizzo.Length; i++)
+            {
+                double bit = Convert.ToInt32(indirizzo[i]);
+
+                if (indirizzo[i] == '1')
+                {
+                    valore += Math.Pow(2, elevazione);
+                }
+
+                elevazione--;
+            }
+
+            return valore;
+           
+        }
+        private void bttn_crea_Click(object sender, EventArgs e)
+        {
+            //CALCOLO CLASSE SOTTORETI
+            int host = int.Parse(InputHost.Text);
+            int sottoreti = int.Parse(InputSottoreti.Text);
+
+            double CDIR;
+
+            double bitRiservatiHost = Math.Ceiling(Math.Log(host, 2)); //BIT PARTE HOST
+            double bitRiservatiSottoreti = Math.Ceiling(Math.Log(sottoreti, 2)); //BIT PARTE SOTTORETE
+
+            double somma = bitRiservatiHost + bitRiservatiSottoreti;
+
+            if (somma <= 8) //CLASSE C
+            {
+               lst_tabella.Items.Add("Classe C");
+               lst_tabella.Items.Add("192.168.0.0");
+               lst_tabella.Items.Add($"255.255.255.{ Subnet(bitRiservatiSottoreti)}");
+                CDIR = 24 + bitRiservatiSottoreti;
+                lst_tabella.Items.Add($"/{CDIR}");
+            }
+            else if (somma <= 16 && somma>8) //CLASSE B
+            {
+                lst_tabella.Items.Add("Classe B");
+                lst_tabella.Items.Add("172.16.0.0");
+                lst_tabella.Items.Add($"255.255.{Subnet(bitRiservatiSottoreti)}.0");
+                CDIR = 16 + bitRiservatiSottoreti;
+                lst_tabella.Items.Add($"/{CDIR}");
+            }
+            else if(somma>16)//CLASSE A
+            {
+                lst_tabella.Items.Add("Classe A");
+                lst_tabella.Items.Add("10.0.0.0");
+                lst_tabella.Items.Add($"255.{Subnet(bitRiservatiSottoreti)}.0.0");
+                CDIR = 8 + bitRiservatiSottoreti;
+                lst_tabella.Items.Add($"/{CDIR}");
+            }
+         }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Verso maggio con un paggio vo in viaggio. Non vaneggio, nè motteggio; forse è peggio! Se mi seggo, più non reggo: mangio o leggo. Se non fuggo qui mi struggo, ma se fuggo vado al poggio e un alloggio là mi foggio, sotto un faggio, con coraggio.
